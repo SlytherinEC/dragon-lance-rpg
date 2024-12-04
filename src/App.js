3afe50cb77da245c9da0458d28dedcec60c06b62
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-12-04 19:27:10 +0100

    modificaciones en App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
 import './App.css';
 import Pantalla from './components/Pantalla/Pantalla';
-import logoDragonLance from '../src/img/dragonlance-logo.png';
-import Info from './components/Info/Info';
-import datosEnemigos from "./data/datosEnemigos.json";
-import datosHeroes from "./data/datosHeroes.json";
-import Heroe from './components/Heroe/Heroe';
-import { useState } from 'react';
-import logicaJuego from './utils/LogicaJuego';
 import Footer from './components/Footer/Footer';
-
+import Header from './components/Header/Header';
 
 function App() {
-
-  const datosE = datosEnemigos;
-  const datosH = datosHeroes;
-
-  const [estadoJuego, setEstadoJuego] = useState('inicio');
-  const [arrayEnemigos, setArrayEnemigos] = useState([]); 
 
   // Primero iniciamos el juego
 
   return (
     <div className="App">
-      <header className='header'>
-        <img
-          className='dragonlance-logo'
-          src={logoDragonLance}
-          alt='Imagen del logo de DragonLance'
-        />
-      </header>
-      
-      <div className='contenedor-principal'>
-        <div className='contenedor-personajes'>
-          {datosH.map((heroe, index) => (
-            <Heroe key={index} heroe={heroe} />
-          ))}
-        </div>
-        <Pantalla />
-        <div className='contenedor-personajes'>
 
-          <Info
-            Id={logicaJuego.crearAleatorio(0, datosE.length - 1)}
-          />
-          <Info
-            Id={logicaJuego.crearAleatorio(0, datosE.length - 1)}
-          />
-          <Info
-            Id={logicaJuego.crearAleatorio(0, datosE.length - 1)}
-          />
-        </div>
-      </div>
-      <footer className='site-footer'>
-          <Footer />
-      </footer>
+      <Header />
+      <Pantalla />
+      <Footer />
+
     </div>
   );
 }
