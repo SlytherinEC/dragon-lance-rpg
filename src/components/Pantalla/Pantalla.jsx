9174922a54ba1685dcc364c21b13b559c4706a45
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-12-05 02:31:03 +0100

    combate de dos personajes ok

--- src/components/Pantalla/Pantalla.jsx
+++ src/components/Pantalla/Pantalla.jsx
@@ -14,17 +14,28 @@ function Pantalla() {
   const [estadoJuego, setEstadoJuego] = useState('game_start');
   const [equipoHeroes, setEquipoHeroes] = useState([]);
   const [equipoEnemigos, setEquipoEnemigos] = useState([]);
-  const [mensaje, setMensaje] = useState("")
+  const [mensaje, setMensaje] = useState("");
+  const [turnoHeroes, setTurnoHeroes] = useState(true);
+  const [animacionHeroe, setAnimacionHeroe] = useState(false);
+  const [animacionEnemigo, setAnimacionEnemigo] = useState(false);
+  const [animacionEnemigoAtaque, setAnimacionEnemigoAtaque] = useState(false);
+  const [animacionHeroeSacudida, setAnimacionHeroeSacudida] = useState(false);
+  
 
   const iniciarJuego = () => {
-    setEstadoJuego('game_play');
+    setEstadoJuego('game_init');
     setEquipoHeroes(logicaJuego.llenarHeroes(datosHeroes));
     setEquipoEnemigos(logicaJuego.llenarEnemigos(datosEnemigos, 4));
     setMensaje(mensajes.inicio);
   };
 
-  const finalizarJuego = () => {
-    setEstadoJuego('game_end');
+  const jugarJuego = () => {
+    setEstadoJuego('game_play');
+    setMensaje(mensajes.jugar);
+  };
+
+  const pausarjuego = () => {
+    setEstadoJuego('game_pause');
   };
 
   const reiniciarJuego = () => {
@@ -34,7 +45,52 @@ function Pantalla() {
 
   };
 
-
+  const atacarEnemigo = () => {
+    // Iniciar animación del héroe
+    setAnimacionHeroe(true);
+  
+    // Después de la animación del héroe, iniciar la sacudida del enemigo
+    setTimeout(() => {
+      setAnimacionHeroe(false);
+      setAnimacionEnemigo(true);
+  
+      // Actualizar vida del enemigo
+      const nuevosEnemigos = [...equipoEnemigos];
+      logicaJuego.atacarTarget(equipoHeroes[0], nuevosEnemigos[3]);
+      setEquipoEnemigos(nuevosEnemigos);
+  
+      // Detener la sacudida del enemigo y actualizar mensaje
+      setTimeout(() => {
+        setAnimacionEnemigo(false);
+        setMensaje(`El enemigo ${nuevosEnemigos[3].nombre} ha recibido daño.`);
+  
+        // Esperar 1 segundo antes de que el enemigo ataque
+        setTimeout(() => {
+          // Iniciar animación del enemigo atacando
+          setAnimacionEnemigoAtaque(true);
+  
+          // Después de la animación del enemigo, iniciar sacudida del héroe
+          setTimeout(() => {
+            setAnimacionEnemigoAtaque(false);
+            setAnimacionHeroeSacudida(true);
+  
+            // Actualizar vida del héroe
+            const nuevosHeroes = [...equipoHeroes];
+            logicaJuego.atacarTarget(nuevosEnemigos[3], nuevosHeroes[0]);
+            setEquipoHeroes(nuevosHeroes);
+  
+            setMensaje(`${nuevosHeroes[0].nombre} ha recibido daño del enemigo.`);
+  
+            // Detener la sacudida del héroe
+            setTimeout(() => {
+              setAnimacionHeroeSacudida(false);
+            }, 500); // Duración de la sacudida del héroe
+          }, 300); // Duración del desplazamiento del enemigo
+        }, 1000); // Espera de 1 segundo antes del ataque del enemigo
+      }, 500); // Duración de la sacudida del enemigo
+    }, 300); // Duración del desplazamiento del héroe
+  };
+    
   console.log(equipoEnemigos);
   console.log(equipoHeroes);
 
@@ -44,8 +100,7 @@ function Pantalla() {
 
       {estadoJuego === 'game_start' && <button className="btn-inicio" onClick={iniciarJuego}>Iniciar Juego</button>}
 
-      {estadoJuego === 'game_play' &&
-
+      {estadoJuego === 'game_init' &&
         <>
 
           <div className="contenedor-fila superior">
@@ -53,20 +108,37 @@ function Pantalla() {
               <Mensajes texto={mensaje} />
             </div>
 
-          </div>
-          <div className="contenedor-fila inferior">
-            <div className='contenedor-personaje heroe'>
-
-              <Heroe heroe={equipoHeroes[logicaJuego.crearAleatorio(0, equipoHeroes.length - 1)]} />
-            </div>
 
             <div className="contenedor-boton">
-              <button className="btn-inicio" onClick={reiniciarJuego}>Reiniciar</button>
+              <button className="btn-inicio" onClick={jugarJuego}>Iniciar</button>
+            </div>
+          </div>
+
+        </>
+      }
+
+      {estadoJuego === 'game_play' &&
+
+        <>
+
+          <div className="contenedor-fila superior">
+            <div className="contenedor-mensajes">
+              <Mensajes texto={mensaje} />
+              <div className="contenedor-boton">
+                <button className="btn-inicio" onClick={atacarEnemigo}>Atacar</button>
+                <button className="btn-inicio" onClick={reiniciarJuego}>Reiniciar</button>
+              </div>
+            </div>
+          </div>
+          <div className="contenedor-fila inferior">
+          <div className={`contenedor-personaje heroe ${animacionHeroe ? 'heroe-animado' : ''} ${animacionHeroeSacudida ? 'heroe-sacudida' : ''}`}>
+
+              {/* <Heroe heroe={equipoHeroes[logicaJuego.crearAleatorio(0, equipoHeroes.length - 1)]} /> */}
+              <Heroe heroe={equipoHeroes[0]} />
             </div>
 
-
-            <div className="contenedor-personaje enemigo">
-              <Enemigo enemigo={equipoEnemigos[3]} />
+            <div className={`contenedor-personaje enemigo ${animacionEnemigo ? 'enemigo-sacudida' : ''} ${animacionEnemigoAtaque ? 'enemigo-animado' : ''}`}>
+            <Enemigo enemigo={equipoEnemigos[3]} />
             </div>
 
           </div>
--- src/utils/LogicaJuego.js
+++ src/utils/LogicaJuego.js
@@ -31,8 +31,8 @@ const logicaJuego = {
         datosEnemigos[indice].nombre,
         datosEnemigos[indice].arma,
         datosEnemigos[indice].ataque = logicaJuego.crearAleatorio(7, 10), // Ataque aleatorio entre 7 y 10.
-        datosEnemigos[indice].defensa = logicaJuego.crearAleatorio(3, 6), // Defensa aleatoria entre 1 y 5.
-        datosEnemigos[indice].vida = logicaJuego.crearAleatorio(25, 40), // Vida aleatoria entre 25 y 40.
+        datosEnemigos[indice].defensa = logicaJuego.crearAleatorio(3, 8), // Defensa aleatoria entre 1 y 5.
+        datosEnemigos[indice].vida = logicaJuego.crearAleatorio(50, 80), // Vida aleatoria entre 25 y 40.
         datosEnemigos[indice].recompensa,
         datosEnemigos[indice].imagen
       );
@@ -85,9 +85,108 @@ const logicaJuego = {
     }
     return heroes;
   },
-  decirHola: () => {
-    return 'El juego ha comenzado';
+  atacarTarget: (atacante, atacado) => {
+    // Inicializamos el objeto 'arma' que contendrá el nombre y el daño del arma utilizada en el ataque.
+    let arma = {};
+
+    // Si el atacante es un héroe, se selecciona un arma del equipamiento del héroe.
+    if (atacante.equipo === 'heroes') {
+      // Llamamos a la función 'elegirArma' que selecciona aleatoriamente un arma del héroe (puede ser principal o secundaria).
+      arma = logicaJuego.elegirArma(atacante);
+    } else {
+      // Si el atacante es un enemigo, asignamos su arma predefinida al objeto 'arma' con su nombre y daño.
+      arma.nombre = atacante.arma;  // Se asigna el nombre del arma del enemigo.
+      arma.dano = atacante.ataque;  // Se asigna el valor del daño del enemigo.
+    }
+
+    // Realizamos el ataque llamando a la función 'ataque', que procesa el daño y los efectos del ataque.
+    logicaJuego.ataque(atacante, arma, atacado);
   },
+  elegirArma: (objeto) => {
+    // Generamos un número aleatorio entre 1 y 10 para determinar qué arma se seleccionará.
+    let aleatorio = logicaJuego.crearAleatorio(1, 10);
+
+    // Inicializamos el objeto 'arma' que contendrá la información del arma seleccionada.
+    const arma = {};
+
+    // Si el número generado es par, seleccionamos el arma principal del héroe.
+    if (aleatorio % 2 === 0) {
+      arma.nombre = objeto.equipamiento.armas.principal.nombre;
+      arma.dano = objeto.equipamiento.armas.principal.dano + objeto.equipamiento.ataque;
+    } else {
+      // Si el número es impar, seleccionamos el arma secundaria del héroe.
+      arma.nombre = objeto.equipamiento.armas.secundaria.nombre;
+      arma.dano = objeto.equipamiento.armas.secundaria.dano + objeto.equipamiento.ataque;
+    }
+
+    // Devolvemos el objeto 'arma' con el nombre y el daño del arma seleccionada.
+    return arma;
+  },
+  ataque: (atacante, arma, atacado) => {
+
+    // Variable que almacenará el valor de daño calculado en el ataque.
+    let dano;
+
+    // Comprobamos si el atacante es un héroe. Para esto usamos el atributo 'equipo'.
+    if (atacante.equipo === 'heroes') {
+      // Si es un héroe, calculamos el daño comparando la defensa del enemigo con el daño del arma.
+      // El daño se obtiene restando el valor de defensa del enemigo al daño del arma.
+      dano = atacado.defensa - arma.dano;
+
+      // Actualizamos los puntos de vida del enemigo tras el ataque. Si el valor de 'dano' es positivo,
+      // significa que la defensa del enemigo no fue suficiente para bloquear todo el daño, por lo que 
+      // se reduce la vida en consecuencia.
+      atacado.vida += dano;
+
+    } else {
+      // Si el atacante es un enemigo, se realiza la misma lógica pero aplicada a los héroes.
+      // Aquí se compara la defensa del héroe con el daño del arma del enemigo.
+      dano = atacado.equipamiento.defensa - arma.dano;
+
+      if (dano < 0) {
+        // Se actualizan los puntos de vida del héroe atacado, restando el valor calculado del daño.
+        atacado.vida += dano;
+
+      } else {
+        atacado.equipamiento.defensa -= dano + 1;
+        console.log(`El ataque se estrella contra la ${atacado.equipamiento.armadura}
+            de ${atacado.nombre} y reduce su eficacia en ${dano} puntos`);
+
+      }
+
+    }
+
+    // En caso de que el daño resultante sea menor que 1 (es decir, negativo), lo multiplicamos por -1
+    // para convertirlo en un valor positivo, ya que no tendría sentido que un ataque "cure" al objetivo.
+    if (dano < 1) {
+      dano *= -1;
+    }
+
+    console.log(`${atacante.nombre} ataca a ${atacado.nombre} 
+        con ${arma.nombre} y le hace ${dano} puntos de daño. A 
+        ${atacado.nombre} le quedan ${atacado.vida} puntos de vida.`);
+
+  },
+  verificarVida: (objeto) => {
+
+    let estaVivo = true;
+
+    // Si la vida del objeto es menor o igual a 0, el objeto está muerto.
+    if (objeto.vida <= 0) {
+
+      estaVivo = false;
+    }
+
+    return estaVivo;
+  },
+  seleccionarRecompensa: (enemigo) => {
+    // Seleccionamos una recompensa aleatoria del array de recompensas del enemigo
+    const indiceAleatorio = logicaJuego.crearAleatorio(0, enemigo.recompensa.length - 1);
+    return enemigo.recompensa[indiceAleatorio];
+
+  }
+
+
 }
 
 export default logicaJuego;
