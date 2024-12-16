4f79fdc44fa94788dd25aa4b40af9c3ae11ad605
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-12-16 02:44:14 +0100

    Creado componente Inventario

--- src/components/Inventario/Inventario.jsx
+++ src/components/Inventario/Inventario.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Inventario.css'
 import PersonajeThumbnail from '../PersonajeThumbnail/PersonajeThumbnail'
+import Recompensas from '../Recompensas/Recompensas'
+import Estadisticas from '../Recompensas/Estadisticas'
+import Boton from '../Boton/Boton'
 
+function Inventario({ equipoHeroes }) {
 
-function Inventario({equipoHeroes}) {
+  const [mostrarEstadisticas, setMostrarEstadisticas] = useState(false);
 
+  const mostrarEstadisticasClick = () => {
+    setMostrarEstadisticas(!mostrarEstadisticas);
+  }
 
   return (
-    <div>
-        {equipoHeroes.map((personaje, index) => (
-          <PersonajeThumbnail personaje={personaje} key={index} />
-        ))}
+    <div className='inventario'>
+            <div className="contenedor-boton btn-inventario">
+        <Boton onClick={mostrarEstadisticasClick} texto={mostrarEstadisticas ? "Recompensas" : "Estadísticas"} />
+      </div>
+
+      {equipoHeroes.map((personaje, index) => (
+        <div className="contenedor-tarjeta" key={index}>
+          <PersonajeThumbnail personaje={personaje} />
+          {mostrarEstadisticas ? (
+            <Estadisticas heroe={personaje} />
+          ) : (
+          <Recompensas heroe={personaje}  />
+          )}
+        </div>
+      ))}
     </div>
   )
 }
